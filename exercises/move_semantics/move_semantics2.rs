8e543cc30aea434f8347c11cb1d460bec5fe4617
Emilio DE SOUSA <emilio.desousa@sicara.com>
2023-02-12 13:07:56 +0100

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:exercises/structs/structs3.rs

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -1,13 +1,10 @@
 // move_semantics2.rs
 // Make me compile without changing line 13 or moving line 10!
 // Execute `rustlings hint move_semantics2` or use the `hint` watch subcommand for a hint.
-
-// I AM NOT DONE
-
 fn main() {
     let vec0 = Vec::new();
 
-    let mut vec1 = fill_vec(vec0);
+    let mut vec1 = fill_vec(vec0.to_vec());
 
     // Do not change the following line!
     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
@@ -19,10 +16,8 @@ fn main() {
 
 fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
     let mut vec = vec;
-
     vec.push(22);
     vec.push(44);
     vec.push(66);
-
     vec
 }
